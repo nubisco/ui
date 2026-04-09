cad8008e67caccfe232dfdeaefa548c0cfbcb32a
José Silva <jose@nubisco.io>
2026-04-09 13:37:21 +0100

    fix: register components explicitly to fix global registration in library builds
    import.meta.glob with eager:true compiles to an empty object in library mode,
    so app.component() was never called and no Nb* components were registered globally.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,54 @@
 import type { App } from 'vue'
+import NbBadge from './Badge.vue'
+import NbBreadcrumbs from './Breadcrumbs.vue'
+import NbButton from './Button.vue'
+import NbCheckbox from './Checkbox.vue'
+import NbColorStrip from './ColorStrip.vue'
+import NbFlag from './Flag.vue'
+import NbGrid from './Grid.vue'
+import NbIcon from './Icon.vue'
+import NbJsonTree from './JsonTree.vue'
+import NbLabel from './Label.vue'
+import NbMessage from './Message.vue'
+import NbModal from './Modal.vue'
+import NbNumberInput from './NumberInput.vue'
+import NbPanel from './Panel.vue'
+import NbRadio from './Radio.vue'
+import NbSelect from './Select.vue'
+import NbShell from './Shell.vue'
+import NbSidebarLink from './SidebarLink.vue'
+import NbSlider from './Slider.vue'
+import NbTextInput from './TextInput.vue'
+import NbToast from './Toast.vue'
 
-const components: Record<string, any> = import.meta.glob('./**/*.vue', {
-  eager: true,
-})
+const components = {
+  NbBadge,
+  NbBreadcrumbs,
+  NbButton,
+  NbCheckbox,
+  NbColorStrip,
+  NbFlag,
+  NbGrid,
+  NbIcon,
+  NbJsonTree,
+  NbLabel,
+  NbMessage,
+  NbModal,
+  NbNumberInput,
+  NbPanel,
+  NbRadio,
+  NbSelect,
+  NbShell,
+  NbSidebarLink,
+  NbSlider,
+  NbTextInput,
+  NbToast,
+}
 
 export default {
   install(app: App) {
-    Object.keys(components).forEach((key) => {
-      if (key.includes('/labs/')) return
-
-      const name = `Nb${key.replace(/(.+\/)(\w+)(\.vue)$/, '$2')}`
-      const component = components[key]
-      app.component(name, component.default)
+    Object.entries(components).forEach(([name, component]) => {
+      app.component(name, component)
     })
   },
 }
