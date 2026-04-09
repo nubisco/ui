4faef005122151dbdcde34a52e32eb4a47aa84fa
José Silva <jose@nubisco.io>
2026-04-09 13:44:26 +0100

    fix: add missing components to global registration (FileUploader, ImageCropper, AiLabel, Switch, Form)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,16 @@
 import type { App } from 'vue'
+import NbAiLabel from './AiLabel.vue'
 import NbBadge from './Badge.vue'
 import NbBreadcrumbs from './Breadcrumbs.vue'
 import NbButton from './Button.vue'
 import NbCheckbox from './Checkbox.vue'
 import NbColorStrip from './ColorStrip.vue'
+import NbFileUploader from './FileUploader.vue'
 import NbFlag from './Flag.vue'
+import NbForm from './Form.vue'
 import NbGrid from './Grid.vue'
 import NbIcon from './Icon.vue'
+import NbImageCropper from './ImageCropper.vue'
 import NbJsonTree from './JsonTree.vue'
 import NbLabel from './Label.vue'
 import NbMessage from './Message.vue'
@@ -18,18 +22,23 @@ import NbSelect from './Select.vue'
 import NbShell from './Shell.vue'
 import NbSidebarLink from './SidebarLink.vue'
 import NbSlider from './Slider.vue'
+import NbSwitch from './Switch.vue'
 import NbTextInput from './TextInput.vue'
 import NbToast from './Toast.vue'
 
 const components = {
+  NbAiLabel,
   NbBadge,
   NbBreadcrumbs,
   NbButton,
   NbCheckbox,
   NbColorStrip,
+  NbFileUploader,
   NbFlag,
+  NbForm,
   NbGrid,
   NbIcon,
+  NbImageCropper,
   NbJsonTree,
   NbLabel,
   NbMessage,
@@ -41,6 +50,7 @@ const components = {
   NbShell,
   NbSidebarLink,
   NbSlider,
+  NbSwitch,
   NbTextInput,
   NbToast,
 }
